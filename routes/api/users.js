9f89a7e2b394609c532af3d7dae4204491cfeb9b
Anyta Ali <anytarali@gmail.com>
2020-07-27 17:03:45 -0500

    added password hashing with bcryptjs

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+
+const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
 
 //@route    POSTT api/users
 //@desc     Register user
@@ -15,12 +19,39 @@ router.post(
       "please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User Registeration ");
+    const { name, email, password } = req.body;
+    try {
+      //check if User exist
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({
+          errors: [{ msg: "User already exists" }],
+        });
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      //Encrpt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+      //save users in mongodb
+      await user.save();
+
+      //return the response
+      res.send("User registered in database");
+    } catch (err) {
+      console.log(err.message);
+      req.status(500).send("server error");
+    }
   }
 );
 
